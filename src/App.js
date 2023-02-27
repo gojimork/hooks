f178775332349c557b80f255e70cba789e02e45b
gojimor <jgthjgth@gmail.com>
2023-02-27 21:29:41 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import HookSwitcher from "./hook-switcher";
 
-function App() {
-  return <HookSwitcher />;
+export default function App() {
+  const [value, setValue] = useState(1);
+  const [view, setView] = useState(true);
+
+  if (view) {
+    return (
+      <div>
+        <button onClick={() => setView(false)}>hide</button>
+        <button onClick={() => setValue((value) => value + 1)}>+1</button>
+        <PlanetInfo id={value} />
+      </div>
+    );
+  } else {
+    return <button onClick={() => setView(true)}>show</button>;
+  }
 }
-
-export default App;
+const PlanetInfo = ({ id }) => {
+  const [planetName, setPlanetName] = useState("");
+  useEffect(() => {
+    let canceled = false;
+    const url = `https://swapi.dev/api/planets/${id}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((body) => !canceled && setPlanetName(body.name));
+    return () => (canceled = true);
+  }, [id]);
+  return (
+    <div>
+      {id} - {planetName}
+    </div>
+  );
+};
